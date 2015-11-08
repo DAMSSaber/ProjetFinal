b8e10cab685c2714d90e49910052aaab7e36f609
Saber DAMS <saber@mobvalue.com>
2015-11-08 17:10:38 +0100

    add edit profil

--- app/src/main/java/com/ecolemultimedia/projetfinal/activities/EditProfilActivity.java
+++ app/src/main/java/com/ecolemultimedia/projetfinal/activities/EditProfilActivity.java
@@ -1,19 +1,108 @@
 package com.ecolemultimedia.projetfinal.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.ecolemultimedia.projetfinal.R;
+import com.ecolemultimedia.projetfinal.models.User;
+import com.ecolemultimedia.projetfinal.views.ViewMenu;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class EditProfilActivity extends AppCompatActivity {
+
+    RelativeLayout ui_rl_menu = null;
+
+    private EditText ui_username = null;
+    private EditText ui_email = null;
+    private EditText ui_age = null;
+    private EditText ui_sex = null;
+    private Button ui_btn_valider = null;
+
+    private Firebase mFirebaseRef;
+
+    private ImageView user_profil_selfie = null;
+    private TextView username_label = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profil);
 
+
+        ui_username = (EditText) findViewById(R.id.username_label);
+        ui_email = (EditText) findViewById(R.id.email);
+        ui_age = (EditText) findViewById(R.id.age_label);
+        ui_sex = (EditText) findViewById(R.id.sex_label);
+
+
+
+        mFirebaseRef = new Firebase("https://projetfinal.firebaseio.com/");
+
+        ui_btn_valider = (Button) findViewById(R.id.ui_btn_valider);
+
+        user_profil_selfie = (ImageView) findViewById(R.id.user_profil_selfie);
+
+
+        ui_rl_menu = (RelativeLayout) findViewById(R.id.ui_rl_menu);
+        ViewMenu viewMenu = new ViewMenu(this, EditProfilActivity.this);
+        viewMenu.init(2);
+        ui_rl_menu.addView(viewMenu);
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        mFirebaseRef.child("users/" + mFirebaseRef.getAuth().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                // do some stuff once
+                Log.d("•••", "" + snapshot.getValue());
+
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(String.valueOf(snapshot.getValue()));
+                    User currentUser = new User();
+                    currentUser.initUser(jsonObject);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.d("•••", "azazaz");
+            }
+        });
+
+
+        ui_btn_valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               onBackPressed();
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/ecolemultimedia/projetfinal/activities/ProfilActivity.java
+++ app/src/main/java/com/ecolemultimedia/projetfinal/activities/ProfilActivity.java
@@ -5,25 +5,33 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
 import com.ecolemultimedia.projetfinal.R;
-import com.ecolemultimedia.projetfinal.components.AppController;
+import com.ecolemultimedia.projetfinal.models.User;
 import com.ecolemultimedia.projetfinal.views.ViewMenu;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
-import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProfilActivity extends AppCompatActivity {
 
     private ImageView mUserProfilSelfie;
-    RelativeLayout ui_rl_menu=null;
+    RelativeLayout ui_rl_menu = null;
     private Firebase mFirebaseRef;
+
+    private TextView ui_username = null;
+    private TextView ui_email = null;
+    private TextView ui_age = null;
+    private TextView ui_sex = null;
+    private Button ui_btn_valider = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,48 +39,23 @@ public class ProfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profil);
 
 
-        ui_rl_menu=(RelativeLayout)findViewById(R.id.ui_rl_menu);
-        ViewMenu viewMenu= new ViewMenu(this,ProfilActivity.this);
+        ui_rl_menu = (RelativeLayout) findViewById(R.id.ui_rl_menu);
+        ViewMenu viewMenu = new ViewMenu(this, ProfilActivity.this);
         viewMenu.init(2);
         ui_rl_menu.addView(viewMenu);
+
+
+        ui_username = (TextView) findViewById(R.id.username);
+        ui_email = (TextView) findViewById(R.id.email);
+        ui_age = (TextView) findViewById(R.id.age);
+        ui_sex = (TextView) findViewById(R.id.sex);
+        ui_btn_valider = (Button) findViewById(R.id.ui_btn_valider);
+
+
         mFirebaseRef = new Firebase("https://projetfinal.firebaseio.com/");
 
-        mUserProfilSelfie = (ImageView)findViewById(R.id.user_profil_selfie);
+        mUserProfilSelfie = (ImageView) findViewById(R.id.user_profil_selfie);
 
-
-
-        mFirebaseRef.child("users/").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                // do some stuff once
-                Log.d("•••", "" + snapshot.getValue());
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-                Log.d("•••", "azazaz");
-            }
-        });
-
-
-        /*
-        String userProfilSelfieUrl = "http://1.bp.blogspot.com/-zuajEFAjpmw/VVwsMcfHyPI/AAAAAAAABHk/T5r-HYMy4No/s1600/Screenshot_2015-05-20-10-55-23.png";
-        AppController.getInstance().getRequestQueue().getCache().remove(userProfilSelfieUrl);
-        ImageLoader imageLoader = AppController.getInstance().getImageLoader();
-        imageLoader.get(userProfilSelfieUrl, new ImageLoader.ImageListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-            @Override
-            public void onResponse(ImageLoader.ImageContainer response, boolean arg1) {
-                if (response.getBitmap() != null) {
-
-                    mUserProfilSelfie.setImageBitmap(response.getBitmap());
-                }
-            }
-        });
-        */
     }
 
     public void disconnect(View view) {
@@ -87,4 +70,61 @@ public class ProfilActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        mFirebaseRef.child("users/" + mFirebaseRef.getAuth().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                // do some stuff once
+                Log.d("•••", "" + snapshot.getValue());
+
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(String.valueOf(snapshot.getValue()));
+                    User currentUser = new User();
+                    currentUser.initUser(jsonObject);
+
+
+                    if (currentUser.getUsername() != null) {
+                        ui_username.setText(currentUser.getUsername());
+
+                    }
+                    if (currentUser.getEmail() != null) {
+                        ui_email.setText(currentUser.getEmail());
+                    }
+                    if (currentUser.getBirthdate() != null) {
+                        ui_age.setText(currentUser.getBirthdate());
+                    }
+                    if (currentUser.getSex() != null) {
+                        ui_sex.setText(currentUser.getSex());
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.d("•••", "azazaz");
+            }
+        });
+
+
+        ui_btn_valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilActivity.this, EditProfilActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
